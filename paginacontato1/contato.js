15b2b663e6062f9a24e0152b871b79b2a6c4959a
Letícia Ferreira da Silva <114198662+Leehfer@users.noreply.github.com>
2022-11-08 21:12:39 -0300

    Add files via upload

---
+++ paginacontato1/contato.js
@@ -0,0 +1,11 @@
+function abrir() {
+    if (itens.style.display == 'flex') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'flex'
+    }
+}
+function mudarTamanho() 
+    if (window.innerWidth >= 378) {
+        itens.style.display = 'flex'
+    }
